89d0e6d6a6b814f2a4c86b802416f4f79a8c2668
Web Dev Simplified <webdevsimplified@gmail.com>
2019-10-29 20:54:43 -0500

    Add Newsletter Form

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,4 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "prismjs/themes/prism-okaidia.css"
+import './src/css/recaptcha.css'
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,24 @@ import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 
 class Layout extends React.Component {
+  componentDidMount() {
+    const form = document.body.querySelector('.formkit-sticky-bar')
+    if (form == null) {
+      const script = document.createElement("script")
+
+      script.src = "https://web-dev-simplified.ck.page/23989b36d2/index.js"
+      script.async = true
+      script.dataset.uid = '23989b36d2'
+  
+      document.body.prepend(script)
+    }
+  }
+
+  componentWillUnmount() {
+    const script = document.querySelector('[src="https://web-dev-simplified.ck.page/23989b36d2/index.js"]')
+    if (script != null) script.remove()
+  }
+
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
