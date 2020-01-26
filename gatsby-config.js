6c5eafa7363521427f0cecca30c311369d803fe5
Web Dev Simplified <webdevsimplified@gmail.com>
2020-01-26 12:23:55 -0600

    Add Dynamic Expandable Content Article

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-embedder`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
