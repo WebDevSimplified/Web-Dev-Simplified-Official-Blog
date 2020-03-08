a0aee87f4dce5dad7c7740169b944425c2e4066e
Web Dev Simplified <webdevsimplified@gmail.com>
2020-03-08 14:44:00 -0500

    Fix Redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
