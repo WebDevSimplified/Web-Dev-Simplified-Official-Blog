b55bde43ab671e452024d47fad5932d1c31c749e
Web Dev Simplified <kyle@webdevsimplified.com>
2023-06-09 07:44:21 -0500

    Add Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,12 @@
 // You can disable this by removing "@ts-check" and `@type` comments below.
 import youtubeEmbed from "./src/plugins/youtubeEmbed.mjs"
 import responsiveImages from "./src/plugins/responsiveImages.mjs"
-import codeHighlightPre from "./src/plugins/codeHighlightPre.mjs"
-import preact from "@astrojs/preact" // @ts-check
-import remarkShiki from "@stefanprobst/remark-shiki"
-import * as shiki from "shiki"
-import rangeParser from "parse-numeric-range"
+import myRemarkShiki from "./src/plugins/myRemarkShiki.mjs"
 
 import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx"
-
-const shikiHighlighter = await shiki.getHighlighter({ theme: "dark-plus" })
+import preact from "@astrojs/preact" // @ts-check
 
 // https://astro.build/config
 export default defineConfig(
@@ -29,37 +22,10 @@ export default defineConfig(
     markdown: {
       syntaxHighlight: false,
       // TODO: Add official Shiki integration when line highlighting is supported
-      remarkPlugins: [
-        [
-          remarkShiki,
-          {
-            highlighter: shikiHighlighter,
-            parseMeta,
-          },
-        ],
-        youtubeEmbed,
-        responsiveImages,
-      ],
+      remarkPlugins: [myRemarkShiki, youtubeEmbed, responsiveImages],
     },
   }
 )
-
-function parseMeta(meta, { lang }) {
-  if (lang != null) parseLang(lang)
-
-  if (meta == null) return undefined
-  if (meta.length === 0) return undefined
-
-  return rangeParser(meta.replaceAll(/[{}]/g, "")).map(line => {
-    return { line, classes: ["highlighted-line"] }
-  })
-}
-
-function parseLang(lang) {
-  if (shikiHighlighter.getLoadedLanguages().includes(lang)) return
-
-  console.error(`The language "${lang}" doesn't exist`)
-}
 
 // TODO: Check to see if my plugins can be replaced with official integrations.
 // TODO: Implement experimental assets integration
